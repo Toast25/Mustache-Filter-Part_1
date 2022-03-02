ff7315e6805aaee4c4bc8afdefedac1f25d3b45e
Toast25 <94575226+Toast25@users.noreply.github.com>
2022-03-01 19:44:51 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+}
+function setup(){
+    canvas = canvas.Create(750,600);
+    canvas.center;
+    video = createCapture(VIDEO);
+    video.hide();
+    
+
+}
+function draw(){
+
+}
